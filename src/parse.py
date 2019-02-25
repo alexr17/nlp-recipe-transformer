2ae44653ed37754ae1a236c292faebb75dbdac24
alexr17 <alexanderrhee2021@u.northwestern.edu>
2019-02-25 00:35:24 -0600

    finishing basic html parsing

--- src/parse.py
+++ src/parse.py
@@ -3,18 +3,39 @@ from src.lib.web_api import web_get
 ### import statements above here
 
 
-def parse_html(url):
-	html = web_get(url)
+def parse_html(cooking_url):
+	'''
+	Takes a cooking url (assumed to be allrecipes) and extracts the relevant
+	information: cuisines, ingredients, steps, etc.
+	
+	It returns an object with the information about the recipe.
+	'''
+	html = web_get(cooking_url)
 	soup = BeautifulSoup(html, 'html.parser')
 
 	cuisine_html = soup.select('meta[itemprop="recipeCategory"]')
 	cuisines = []
 	for i in cuisine_html:
-		cuisines.append(i['content'])
+		cuisines.append(i['content'].strip())
 
-	instructions_html = soup.select('span.recipe-ingred_txt.added')
-	instructions = []
-	for i in instructions_html:
-		instructions.append(i.text)
+	ingredients_html = soup.select('span.recipe-ingred_txt.added')
+	ingredients = []
+	for i in ingredients_html:
+		ingredients.append(i.text.strip())
 
-	# TODO: implement this
+	steps_html = soup.select('span.recipe-directions__list--item')
+	steps = []
+	for i in steps_html:
+		steps.append(i.text.strip())
+
+	return {
+		"cuisines": cuisines,
+		"ingredients": ingredients,
+		"steps": steps
+	}
+
+def parse_ingredients(ingredients):
+	'''
+	Takes a list of ingredients
+	'''
+	return False

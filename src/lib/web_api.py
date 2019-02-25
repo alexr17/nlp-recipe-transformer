517e086ac978ad1c42d253c91ca692d56453df8d
alexr17 <alexanderrhee2021@u.northwestern.edu>
2019-02-24 20:25:22 -0600

    cuisines and instructions bs4

--- src/lib/web_api.py
+++ src/lib/web_api.py
@@ -1,8 +1,5 @@
 from urllib.request import urlopen
-from bs4 import BeautifulSoup
 
 def web_get(url):
     html = urlopen(url)
-    soup = BeautifulSoup(html, 'html.parser')
-
-    return soup
+    return html
--- src/parse.py
+++ src/parse.py
@@ -1,10 +1,20 @@
+from bs4 import BeautifulSoup
 from src.lib.web_api import web_get
 ### import statements above here
 
 
 def parse_html(url):
-    html = web_get(url)
-    print(html)
-    print(type(html))
+	html = web_get(url)
+	soup = BeautifulSoup(html, 'html.parser')
 
-    # TODO: implement this
+	cuisine_html = soup.select('meta[itemprop="recipeCategory"]')
+	cuisines = []
+	for i in cuisine_html:
+		cuisines.append(i['content'])
+
+	instructions_html = soup.select('span.recipe-ingred_txt.added')
+	instructions = []
+	for i in instructions_html:
+		instructions.append(i.text)
+
+	# TODO: implement this

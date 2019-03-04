c2543fb05bb0f4421b96b75cc0b139e8f45d8bd2
alexr17 <alexanderrhee2021@u.northwestern.edu>
2019-03-03 23:15:24 -0600

    tinkering with descriptors et al

--- src/parse.py
+++ src/parse.py
@@ -6,6 +6,7 @@ from src.lib.web_api import web_get
 from src.lib.helpers import levenshtein
 # import statements above here
 
+debug = False
 
 def parse_html(cooking_url):
     '''
@@ -143,12 +144,19 @@ food_groups = {
     "secondary_protein": secondary_protein_kw
 }
 def kw_in_food_group_set(ingredient):
+    '''
+    Checks if a string (ingredient) is in one of the ingredient text files
+    '''
     for fg in food_groups:
         if ingredient in food_groups[fg]:
             return fg
     return False
 
 def split_ingredients(ingredients):
+    '''
+    Takes a list of ingredients and parses each ingredient
+    Returns a list of parsed ingredient objects
+    '''
     food_split = {
         "fruit": [],
         "herb": [],
@@ -165,30 +173,35 @@ def split_ingredients(ingredients):
         if food_type:
             ingredient["matched_word"] = item
             food_split[food_type].append(ingredient)
-        else:
-            # split ingredient and iterate
-            sp_ing = item.split(' ')
-            flag = False
-            if len(sp_ing) > 1:
-                for tkn in sp_ing:
-                    food_type = kw_in_food_group_set(tkn)
-                    if food_type:
-                        ingredient["matched_word"] = tkn
-                        food_split[food_type].append(ingredient)
-                        flag = True
-            if not flag:
-            # now match with levenshtein
-                min_lev = float("inf")
-                food_group = ''
-                food_match = ''
-                for fg in food_groups:
-                    lev_score, best_food = best_match(min_lev, food_groups[fg], item)
-                    if lev_score < min_lev:
-                        min_lev = lev_score
-                        food_group = fg
-                        food_match = best_food
-                ingredient["matched_word"] = food_match
-                food_split[food_group].append(ingredient)
+            continue
+        
+        # split ingredient and iterate over each word
+        sp_ing = item.split(' ')
+        if len(sp_ing) > 1:
+            for tkn in sp_ing:
+                food_type = kw_in_food_group_set(tkn)
+                if food_type:
+                    ingredient["matched_word"] = tkn
+                    food_split[food_type].append(ingredient)
+                    break
+            if "matched_word" in ingredient:
+                continue
+
+        # now match with levenshtein
+        min_lev = float("inf")
+        food_group = ''
+        food_match = ''
+        for fg in food_groups:
+            lev_score, best_food = best_match(min_lev, food_groups[fg], item)
+            if lev_score < min_lev:
+                min_lev = lev_score
+                food_group = fg
+                food_match = best_food
+        ingredient["matched_word"] = food_match
+        food_split[food_group].append(ingredient)
+        
+        if debug and len(ingredient) < min_lev * 1.5:
+            print(f"Ingredient: ({ingredient['ingredient']}) matched to ({ingredient['matched_word']}) has a lev score of {min_lev} that is extremely high")
 
     return food_split
 

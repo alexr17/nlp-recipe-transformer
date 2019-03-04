5b9a6d495016022aa3ca64dbb6f06ab1d5dcf851
Itay Golan <itaygolan19@gmail.com>
2019-03-04 11:32:30 -0600

    split methods

--- src/parse_steps.py
+++ src/parse_steps.py
@@ -28,7 +28,10 @@ def parse_tools(steps):
     return list(set(tools_lst))
 
 def parse_methods(steps):
-    methods = []
+    methods = {
+        "primary_methods": [],
+        "secondary_methods": []
+    }
     methods_kw = set([line.strip() for line in open('./src/lib/categories/ingredients/methods.txt')])
     methods2_kw = set([line.strip() for line in open('./src/lib/categories/ingredients/methods2.txt')])
 
@@ -38,26 +41,34 @@ def parse_methods(steps):
     for raw_step in steps:
         step = nltk.word_tokenize(raw_step)
         for s in step:
-            if s in all_methods_kw:
-                methods.append(s)
+            if s in methods_kw:
+                methods['primary_methods'].append(s)
+            if s in methods2_kw:
+                methods['secondary_methods'].append(s)
 
         bigrams = [step[i:] for i in range(2)]
         bigrams = zip(*bigrams)
         bigrams = [" ".join(bigram) for bigram in list(bigrams)]
 
         for bigram in bigrams:
-            if bigram in all_methods_kw:
-                methods.append(bigram)
+            if bigram in methods_kw:
+                methods['primary_methods'].append(bigram)
+            if bigram in methods2_kw:
+                methods['secondary_methods'].append(bigram)
 
     # Remove duplicates
-    return list(set(methods))
+    methods['primary_methods'] = list(set(methods['primary_methods']))
+    methods['secondary_methods'] = list(set(methods['secondary_methods']))
+    return methods
 
 
 def split_steps(steps, ingredients, tools, methods):
     # TODO: add 'hr', 'min', 'second', 'sec' to time pattern
     timePattern = re.compile('(\d+ hour(?:s?) (?:and )?\d+ minute(?:s?))|(\d+ minute(?:s?))|(\d+ hour(?:s?))')
-    temperaturePattern = re.compile('(\d+(?:(?: [dD]egrees)|(?:\u00b0)) (?:(?:[fF]ahrenheit)|(?:[fF])|(?:[Cc]elcius)|(?:[cC])))')
+    temperaturePattern = re.compile('(\d+(?:(?: [dD]egrees)|(?:\u00b0)))')
 
+
+    methods = list(set(methods['primary_methods']) | set(methods['secondary_methods']))
     new_steps = []
     for raw_step in steps:
         step = nltk.word_tokenize(raw_step)
@@ -71,7 +82,8 @@ def split_steps(steps, ingredients, tools, methods):
             "tools": [],
             "methods": [],
             "times": [],
-            "temperature": []
+            "temperature": [],
+            "raw_step": raw_step
         }
 
         for s in step:

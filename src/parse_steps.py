754e1b68c3ec30d2b4c5d69ade30f62ca1956070
Itay Golan <itaygolan19@gmail.com>
2019-03-03 23:22:08 -0600

    start split_steps

--- src/parse_steps.py
+++ src/parse_steps.py
@@ -1,5 +1,6 @@
 import nltk
 import json
+import re
 from src.lib.clean import valid_tkn
 from src.lib.web_api import web_get
 from src.lib.helpers import levenshtein
@@ -27,42 +28,76 @@ def parse_tools(steps):
     return list(set(tools_lst))
 
 def parse_methods(steps):
-    methods = {
-        "primary_methods": [],
-        "secondary_methods": []
-    }
+    methods = []
     methods_kw = set([line.strip() for line in open('./src/lib/categories/ingredients/methods.txt')])
     methods2_kw = set([line.strip() for line in open('./src/lib/categories/ingredients/methods2.txt')])
+
+    all_methods_kw = methods_kw | methods2_kw
 
 
     for raw_step in steps:
         step = nltk.word_tokenize(raw_step)
         for s in step:
-            if s in methods_kw:
-                methods["primary_methods"].append(s)
-            if s in methods2_kw:
-                methods["secondary_methods"].append(s)
+            if s in all_methods_kw:
+                methods.append(s)
 
         bigrams = [step[i:] for i in range(2)]
         bigrams = zip(*bigrams)
         bigrams = [" ".join(bigram) for bigram in list(bigrams)]
 
         for bigram in bigrams:
-            if bigram in methods_kw:
-                methods["primary_methods"].append(bigram)
-            if bigram in methods2_kw:
-                methods["secondary_methods"].append(bigram)
+            if bigram in all_methods_kw:
+                methods.append(bigram)
 
     # Remove duplicates
-    methods["primary_methods"] = list(set(methods["primary_methods"]))
-    methods["secondary_methods"] = list(set(methods["secondary_methods"]))
-    return methods
+    return list(set(methods))
 
 
-def parse_steps(steps):
+def split_steps(steps, ingredients, tools, methods):
     timePattern = re.compile('(\d+ hour(?:s?) (?:and )?\d+ minute(?:s?))|(\d+ minute(?:s?))|(\d+ hour(?:s?))')
     temperaturePattern = re.compile('(\d+(?:(?: [dD]egrees)|(?:\u00b0)) (?:(?:[fF]ahrenheit)|(?:[fF])|(?:[Cc]elcius)|(?:[cC])))')
 
-    pass
+    new_steps = []
+    for raw_step in steps:
+        step = nltk.word_tokenize(raw_step)
+
+        bigrams = [step[i:] for i in range(2)]
+        bigrams = zip(*bigrams)
+        bigrams = [" ".join(bigram) for bigram in list(bigrams)]
+
+        new_step = {
+            "ingredients": [],
+            "tools": [],
+            "methods": [],
+            "times": [],
+            "temperature": []
+        }
+
+        for s in step:
+            if s in ingredients:
+                new_step['ingredients'].append(s)
+            if s in tools:
+                new_step['tools'].append(s)
+            if s in methods and s not in new_step['methods']:
+                new_step['methods'].append(s)
+
+        for bigram in bigrams:
+            if bigram in ingredients:
+                new_step['ingredients'].append(bigram)
+            if bigram in tools:
+                new_step['tools'].append(bigram)
+            if bigram in methods and bigram not in new_step['methods']:
+                new_step['methods'].append(bigram)
+            if timePattern.match(bigram):
+                new_step['times'].append(bigram)
+            if temperaturePattern.match(bigram):
+                new_step['temperature'].append(bigram)
+
+        new_steps.append(new_step)
+
+    return new_steps
+
+
+
 
 

9cf863e8b7fdbb81f986d62e962f42153c489033
alexr17 <alexanderrhee2021@u.northwestern.edu>
2019-03-10 23:37:58 -0500

    adding cuisine steps and title transformations

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def run():
 
 
 if __name__ == '__main__':
-    #run()
+    # run()
     run_cli()
     # change this to be any of
     # test_recipes()
--- src/transform_cuisine.py
+++ src/transform_cuisine.py
@@ -10,9 +10,14 @@ def transform_cuisine_ingredients(recipe, cuisine):
 
         cuisine_food_type = cuisine[food_type]
         for ing in recipe['ingredients'][food_type]:
+            # don't transform ingredients without a unit
+            if ing['measurement'] == '':
+                continue
+    
             # overwrite the changes we made for the ingredient (if any customizations are found)
             if set_custom_ingredient(ing, cuisine['custom']):
                 continue
+
             # special method for protein
             if 'protein' in food_type:
                 set_protein_ingredient(
@@ -26,7 +31,6 @@ def transform_cuisine_ingredients(recipe, cuisine):
                     ing, cuisine_food_type)
             # if the ingredient is already a part of the cuisine, then remove it from the cuisine dict
             else:
-                ing['transformed_ing'] = ing['ingredient']
                 if len(cuisine_food_type):
                     if type(cuisine_food_type) == list:
                         cuisine_food_type.remove(ing['matched_word'])
@@ -37,7 +41,34 @@ def transform_cuisine_steps(recipe, cuisine):
     '''
     Transforms the steps in the recipe for the given cuisine
     '''
+    mapping = generate_ingredient_mapping(recipe['ingredients'])
+    for step in recipe['steps']:
+        # transform ingredients in steps
+        new_ingredients = []
+        for ing in step['ingredients']:
+            if ing in mapping:
+                new_ingredients.append(mapping[ing])
+        step['ingredients'] = new_ingredients
+        
+        # transform raw step
+        for ing in mapping:
+            if ing in step['raw_step']:
+                step['raw_step'] = step['raw_step'].replace(ing, mapping[ing])
+    
+    for ing in mapping:
+        if ing in recipe['title']:
+            recipe['title'] = recipe['title'].replace(ing, mapping[ing])
     return False
+
+def generate_ingredient_mapping(ingredients):
+    '''
+    Generates the ingredient mapping for the recipe
+    '''
+    mapping = {}
+    for food_type in ingredients:
+        for ing in ingredients[food_type]:
+            mapping[ing['matched_word']] = ing['ingredient']
+    return mapping
 
 def set_protein_ingredient(ing, protein_type, cuisine):
     '''
@@ -47,9 +78,7 @@ def set_protein_ingredient(ing, protein_type, cuisine):
     protein_category = protein_json[protein_type.replace(
         '_protein', '')][ing['matched_word']]['category']
     if len(cuisine[protein_type][protein_category]):
-        ing['transformed_ing'] = cuisine[protein_type][protein_category].pop()
-    else:
-        ing['transformed_ing'] = ing['ingredient']
+        ing['ingredient'] = cuisine[protein_type][protein_category].pop()
 
 
 def set_generic_ingredient(ing, mappings):
@@ -59,7 +88,7 @@ def set_generic_ingredient(ing, mappings):
     # if transformations are list
     if type(mappings) == list:
         # remove the first ingredient
-        ing['transformed_ing'] = mappings.pop(0)
+        ing['ingredient'] = mappings.pop(0)
     # if the transformations are a dictionary
     # then try to find each key in the ingredient
     elif type(mappings) == dict:
@@ -87,9 +116,9 @@ def set_generic_ingredient(ing, mappings):
         # now at this point our food_group is the key in mappings
         # that we want to extract the transformed ingredient from
         # print(mappings[food_group])
-        ing['transformed_ing'] = mappings[food_group]['types'].pop(
+        ing['ingredient'] = mappings[food_group]['types'].pop(
             0) + ' ' + food_group
-        if not len(mappings[food_group]):
+        if not len(mappings[food_group]['types']):
             del mappings[food_group]
 
 def set_custom_ingredient(ing, mappings):
@@ -97,6 +126,6 @@ def set_custom_ingredient(ing, mappings):
     Overwrites ingredients with custom mappings
     '''
     if ing['matched_word'] in mappings:
-        ing['transformed_ing'] = mappings[ing['matched_word']]
+        ing['ingredient'] = mappings[ing['matched_word']]
         return True
     return False
